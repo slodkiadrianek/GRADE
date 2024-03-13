b2ab87d2cc8c1109a277c2b3ab1845ba69184049
Adrian Kurek <adikurek11@gmail.com>
2024-03-13 19:55:16 +0100

    13.0.3.24

--- Public/js/notesUse.js
+++ Public/js/notesUse.js
@@ -3,24 +3,51 @@
 const addText = document.querySelector('.add--text')
 const textAreaBox = document.querySelector('.textarea--box')
 const whereAddText = document.querySelector('.where--add--text')
-const sheetsBox = document.querySelector('.sheets--box')
+const listBox = document.querySelector('.list--box-1')
+const leftSide = document.querySelector('.left--side')
+const note  = document.querySelector('.note')
+const sheet = document.querySelector('.sheet')
+
 
 const allNotes = {
-    arkusz1: ''
+    arkusz1: ``
 }
 const createSheet=  function(notes) {
-    sheetsBox.textContent = ''
+    listBox.textContent = ''
 
-    for(let [y,u] of Object.entries(notes)){
+    for(let [key,value] of Object.entries(notes)){
         const html =`
-        <article class="sheet-box">
-                <h1>${y}</h1>
-                <p>${u}</p>
-            </article>`
-        sheetsBox.insertAdjacentHTML('afterbegin', html)
-        
+        <li class="list--element">
+        <div class="left--side">
+        <div class="ball"></div>
+        <p class="list--text">${key}</p>
+      </div>
+      
+      </li>`
+        listBox.insertAdjacentHTML('afterbegin', html)
     }
 }
+
+// listBox.addEventListener('click', (e) =>{
+//     const target = e.target
+//     console.log(target);
+//     if( target.classList.contains('list--text')){
+//         target.classList.toggle('sheet--selected')
+//         target.previousElementSibling.classList.toggle('done--ball')
+//         note.textContent = allNotes[target.textContent]        
+//     }
+// })
+
+createSheet(allNotes)
+const listOfSheerts = document.querySelectorAll('.list--text')
+
+listOfSheerts.forEach(el =>{
+    el.addEventListener('click', () =>{
+        el.classList.toggle('sheet--selected')
+        el.previousElementSibling.classList.toggle('done--ball')
+        note.textContent = allNotes[el.textContent]
+    })
+})
 
 
 addText.addEventListener('click', ()=>{
@@ -35,13 +62,17 @@ addText.addEventListener('click', ()=>{
             `
             whereAddText.insertAdjacentHTML('afterbegin', html)
             createSheet(allNotes)
+            textAreaBox.value = ``
+
         }else{ console.log('istnieje')}
        }        
     }else{           
     allNotes[whichSheet] += textAreaBox.value
     createSheet(allNotes)
-    }
-   
-    
+    textAreaBox.value = ``
+    } 
 })
+
+console.log('asdasd\nsdadad');
+
 
